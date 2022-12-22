4328d43e85e6a88745ffca5f424dcef3c6614eec
quri34 <71138688+quri34@users.noreply.github.com>
2022-12-22 12:27:57 +0200

    Add files via upload

---
+++ lab10.1/lab10.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+
+bool hasTriple(const std::string& filename)
+{
+    std::ifstream file(filename);
+    std::string line;
+    while (std::getline(file, line))
+    {
+        for (size_t i = 0; i < line.size() - 2; ++i)
+        {
+            if (line[i] == '+' && line[i + 1] == '-' && line[i + 2] == '=')
+            {
+                return true;
+            }
+            if (line[i] == '+' && line[i + 1] == '-' && line[i + 2] == '=')
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    std::string filename = "t.txt";
+    bool result = hasTriple(filename);
+    std::cout << std::boolalpha << result << std::endl;
+    return 0;
+}
